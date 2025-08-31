34ecf1ffefea5d5f2752b41d587cabc8ad5ae1e9
Harsh <hk755063@gmail.com>
2025-09-01 04:42:29 +0530

    singleton-config

--- SOLID/ex06/src/Aviary.java
+++ SOLID/ex06/src/Aviary.java
@@ -1,3 +1,3 @@
-class Aviary {public 
-    void release(Bird b){ b.fly(); System.out.println("Released"); }
+class Aviary {
+    public void release(Bird b){ b.fly(); System.out.println("Released"); }
 }
--- singleton-config/src/com/example/config/AppSettings.java
+++ singleton-config/src/com/example/config/AppSettings.java
@@ -11,11 +11,27 @@ import java.util.Properties;
  */
 public class AppSettings implements Serializable {
     private final Properties props = new Properties();
+    private static AppSettings instance;
 
-    public AppSettings() { } // should not be public for true singleton
+    private AppSettings() { 
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance()!");
+        }
+    } // should not be public for true singleton
 
     public static AppSettings getInstance() {
-        return new AppSettings(); // returns a fresh instance (bug)
+        if(instance == null) {
+            synchronized(AppSettings.class) {
+                if(instance == null) {
+                    instance = new AppSettings();
+                }
+            }
+        }
+        return instance; // returns a fresh instance (bug)
+    }
+
+    protected Object readResolve() {
+        return instance;
     }
 
     public void loadFromFile(Path file) {
